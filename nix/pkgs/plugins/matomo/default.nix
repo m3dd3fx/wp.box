70f33d0c363201ca6e9336906b6b07bb9595faa9
m3dd3fx <marco.medde@proton.me>
2024-06-05 19:20:33 +0200

    updated hash matomo

--- nix/pkgs/plugins/matomo/default.nix
+++ nix/pkgs/plugins/matomo/default.nix
@@ -5,7 +5,7 @@ pkgs.stdenv.mkDerivation rec {
   version = "1.0.30";
   src = pkgs.fetchzip {
     url = "https://downloads.wordpress.org/plugin/wp-piwik.${version}.zip";
-    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    hash = "sha256-t49CXIDmZtv1iDOzyHyd0JHwTdXFM78dKZnjZcXErFw=";
   };
   installPhase = "mkdir -p $out; cp -R * $out/";
 }
